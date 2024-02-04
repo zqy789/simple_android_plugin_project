67fcb10c2987de36cb3444c243ca224ca963b5e8
zhangqiuyang <zqy789@126.com>
2024-02-04 20:42:28 +0800

    配置插件项目工程

--- plugin_scheme/src/main/java/com/example/plugin_scheme/BasePluginActivityDelegate.kt
+++ plugin_scheme/src/main/java/com/example/plugin_scheme/BasePluginActivityDelegate.kt
@@ -1,41 +1,13 @@
 package com.example.plugin_scheme
 
 import android.app.Activity
-import android.os.Bundle
-import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 
-open class BasePluginActivityDelegate : IPluginActivity {
+open class BasePluginActivityDelegate :AppCompatActivity() {
     private lateinit var context: Activity
 
-    override fun attach(proxyActivity: Activity) {
+    fun attach(proxyActivity: Activity) {
         context = proxyActivity
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-
-    }
-
-    override fun onStart() {
-
-    }
-
-    override fun onResume() {
-
-    }
-
-    override fun onPause() {
-
-    }
-
-    override fun onStop() {
-
-    }
-
-    override fun onDestroy() {
-
-    }
-
-    fun setContentView(@LayoutRes layoutId: Int) {
-        context.setContentView(layoutId)
-    }
 }
